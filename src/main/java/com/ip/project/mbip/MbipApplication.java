b76bd3da64d3d6eb278ba4e52bf589314ff9e275
jessLijie <wong.lijie1105@gmail.com>
2023-12-18 12:04:55 +0800

    try

---
+++ src/main/java/com/ip/project/mbip/MbipApplication.java
@@ -0,0 +1,13 @@
+package com.ip.project.mbip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbipApplication.class, args);
+	}
+
+}
